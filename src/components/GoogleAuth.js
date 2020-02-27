016bf547be3df14a6e2fd7fcc5814a5a87ec26f0
allison <allisonchen1130@gmail.com>
2020-02-27 15:16:26 -0500

    google oauth set up

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  // when app first loads, not sure if user singed in or not, null
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "486376040858-a12rg96922a3f7etulmfbo4run9apq4c.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton = () => {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.onSignInClick} className="ui blue google button">
+          <i className="google icon" />
+          Sign in with Google account
+        </button>
+      );
+    }
+  };
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import GoogleAuth from "./GoogleAuth";
+
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
       <Link to="/">Streamer</Link>
       <div className="right menu">
         <Link to="/">Broadcasting streams</Link>
+        <GoogleAuth />
       </div>
     </div>
   );
